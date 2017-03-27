1a45d2cec45b61137e1bd4fc0f0b7dbe421c4ff2
Justin Brazeau <justinb@live.ca>
2017-03-27 10:02:55 +0700

    added analytics

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import App from './app.jsx';
-render( <App/>, document.querySelector("#app"));
+render( <App />, document.querySelector("#app"));
